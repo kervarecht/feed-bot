b6a52932cb8e91a1fe76154b87bce899d969e0e9
Thibault <kervarecht@gmail.com>
2018-09-03 00:13:35 -0400

    Hooked in twitter

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const cors = require("cors");
 const nodemon = require("nodemon");
 const request = require("request");
 const sqlite = require("sqlite3");
+const twit = require("twit")
 
 
 //--EXPRESS CONFIG  --//
@@ -15,3 +16,34 @@ const app = express();
 dotenv.config();
 app.use(cors());
 app.use(bodyparser.urlencoded({extended: false}));
+
+//--TWITTER API CONFIG --//
+
+const twitterTokens = {
+    consumer_key : process.env.TWITTER_API_KEY,
+    consumer_secret : process.env.TWITTER_SECRET_KEY,
+    access_token : process.env.TWITTER_ACCESS_TOKEN,
+    access_token_secret : process.env.TWITTER_ACCESS_SECRET
+}
+const twitterURL = "https://api.twitter.com/1.1/search/tweets.json?q=";
+const T = new twit(twitterTokens);
+
+//-- WATSON API CONFIG --//
+const watsonCredential = process.env.WATSON_CREDENTIAL;
+const watsonURL = process.env.WATSON_URL;
+
+
+//-- TWITTER CALLS --//
+const search = "#Lakers";
+const params = {
+    q: search,
+    count: 25
+}
+
+T.get("/search/tweets", params, function(err, result){
+    const responseText = []
+    if (err){
+        console.log(err);
+    }
+    console.log(result);
+})
